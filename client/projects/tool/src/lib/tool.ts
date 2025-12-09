8f03098022e3e6f951d89205dfad509cb29d9bea
Reza Taba <mrtabaa@gmail.com>
2025-12-10 00:56:07 +0300

    Init an Angular workspace and projects

---
+++ client/projects/tool/src/lib/tool.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-tool',
+  imports: [],
+  template: `
+    <p>
+      tool works!
+    </p>
+  `,
+  styles: ``,
+})
+export class Tool {
+
+}
